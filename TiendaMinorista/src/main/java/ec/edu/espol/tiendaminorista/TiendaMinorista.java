b68f40cb2c13956c4a3297ba9cfd01e657d30dc5
Hilda Victoria Angulo Borja <134457751+vic28code@users.noreply.github.com>
2024-07-25 20:15:09 -0500

    Update TiendaMinorista.java

--- TiendaMinorista/src/main/java/ec/edu/espol/tiendaminorista/TiendaMinorista.java
+++ TiendaMinorista/src/main/java/ec/edu/espol/tiendaminorista/TiendaMinorista.java
@@ -11,6 +11,37 @@ package ec.edu.espol.tiendaminorista;
 public class TiendaMinorista {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Ejemplo de uso del Patrón Iterator
+        SistemaDeTarjetas sistemaDeTarjetas = new SistemaDeTarjetas();
+        sistemaDeTarjetas.agregarTarjeta(new TarjetaCredito("Básica", 50, 1000));
+        sistemaDeTarjetas.agregarTarjeta(new TarjetaCredito("Premium", 100, 5000));
+        sistemaDeTarjetas.agregarTarjeta(new TarjetaCredito("VIP", 200, 10000));
+
+        Iterator<TarjetaCredito> iterator = sistemaDeTarjetas.getIterator();
+        while (iterator.hasNext()) {
+            TarjetaCredito tarjeta = iterator.next();
+            System.out.println("Tipo: " + tarjeta.tipo + ", Costo Anual: " + tarjeta.costoAnual + ", Límite de Crédito: " + tarjeta.limiteCredito);
+        }
+
+        // Ejemplo de uso del Patrón Strategy
+        PlataformaAtencionCliente plataforma = new PlataformaAtencionCliente();
+        plataforma.setCanalAtencion(new CanalWeb());
+        plataforma.atender();
+
+        plataforma.setCanalAtencion(new CanalMovil());
+        plataforma.atender();
+
+        plataforma.setCanalAtencion(new CanalTelefonico());
+        plataforma.atender();
+
+        // Ejemplo de uso del Patrón Chain of Responsibility
+        NotificacionHandler smsHandler = new NotificacionSMS();
+        NotificacionHandler correoHandler = new NotificacionCorreo();
+        NotificacionHandler adicionalHandler = new NotificacionAdicional();
+
+        smsHandler.setSiguiente(correoHandler);
+        correoHandler.setSiguiente(adicionalHandler);
+
+        smsHandler.manejarNotificacion("Recordatorio de pago de tarjeta de crédito.");
     }
 }
